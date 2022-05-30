33caa10b72f5aa65b42a367dc6dba303acf44257
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2022-05-30 09:49:12 +0900

    Amadeus v2.00

--- amadeus.py
+++ amadeus.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import re,os,time,random
 import jaconv
+import urllib.request
 
-# amadeus v1.00
+from selenium import webdriver
+
+# amadeus v2.00
 class WorkInfo:
     def __init__(self,url=''):
         if(url==''):
@@ -20,6 +23,7 @@ class WorkInfo:
             self.genres=[]
             self.imgurl=''
             self.description=''
+            self.sample_url=''
         else:
             response = requests.get(url)
             soup = BeautifulSoup(response.text, "html.parser")
@@ -37,10 +41,11 @@ class WorkInfo:
             self.genres=detail[6]
             self.imgurl=self.get_imgurl(soup)
             self.description=self.get_description(soup)
+            self.sample_url=self.get_sampleurl(soup)
         self.confidence=100.0
         self.maintext=''
         self.remark=''
-        print('コンストラクタを生成しました。')
+        print('WorkInfo コンストラクタを生成しました。')
 
     def remove_end_spaces(self,str):
         if(str[0]==' ' or str[0]=='　'):
@@ -99,6 +104,13 @@ class WorkInfo:
         elems=soup.find("div",attrs={'itemprop':'description'})
         description=elems.get_text()
         return description
+    def get_sampleurl(self,soup):
+        dw_link=''
+        elems=soup.find('a',attrs={'class':'btn_trial'})
+        if(elems):
+            dw_link=elems['href']
+            dw_link='https:' + dw_link
+        return dw_link
     def write2txt(self,filepath):
         #入力されたパスにテキストファイルで作品情報を書き出す。
         with open(filepath+'info.txt','w',encoding='utf-8')as f:
@@ -123,7 +135,6 @@ class WorkInfo:
             f.write('description_separate_point\n')
             f.write(self.description)
         print('Complete writing a txt.')
-
 
     def initialize_by_txt(self,txt):
         for x in txt.splitlines():
@@ -153,5 +164,163 @@ class WorkInfo:
             if(x.split('@:')[0]=='confidence'):
                 self.confidence=float( x.split('@:')[1] )
         self.description=self.title+'\n'+txt.split('description_separate_point')[-1]
+    
+    def download_sample(self,dirpath):
+        if(re.search('ボイス・ASMR',self.type)):
+            os.makedirs(os.path.join(dirpath,self.circle),exist_ok=True)
+            if(self.sample_url):
+                filename=os.path.basename(self.sample_url)
+                urllib.request.urlretrieve(self.sample_url, os.path.join(dirpath,self.circle,filename) )
+                print('Download '+filename)
+                return 'sample'
+            else:
+                ins=m4a_tools(self.url)
+                if(ins.chobit_url!=''):
+                    ins.download(os.path.join(dirpath,self.circle))
+                    return 'm4a'
+                else:
+                    return 'fail'
+        else:
+            return 'Not_voice'
+
+class CircleInfo:
+    def __init__(self,url):
+        self.url=url
+        response = requests.get(url+'/show_type/3')
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        self.circle=self.get_circleName(soup)
+        self.totalworks=self.get_totalworks(soup)
+        self.urls=self.get_list_workurls(soup)
+
+    def print(self):
+        print('サークル名 : '+self.circle)
+        print('URL : :'+self.url)
+        print('作品数 : '+str(self.totalworks))
+        print('作品URLリスト : ')
+        print(self.urls)
+    
+    def download_all(self,dirpath):
+        self.sample=[]
+        self.m4a=[]
+        self.not_voice=[]
+        self.fail=[]
+        print('Download starting the '+ self.circle)
+        for url in self.urls:
+            time.sleep(random.randint(45,75))
+            w1=WorkInfo(url)
+            dl_status=w1.download_sample(dirpath)
+            if(dl_status=='sample'):
+                (self.sample).append(url)
+            elif(dl_status=='m4a'):
+                (self.m4a).append(url)
+            elif(dl_status=='not_voice'):
+                (self.not_voice).append(url)
+            else:
+                (self.fail).append(url)
+        with open(os.path.join(dirpath,self.circle,'dl_info.txt'),'w',encoding='utf-8') as f:
+            f.write('sample : {0}\n'.format(len(self.sample)))
+            [f.write(i+'\n') for i in self.sample]
+            f.write('\nm4a : {0}\n'.format(len(self.m4a)))
+            [f.write(i+'\n') for i in self.m4a]
+            f.write('\nnot_voice : {0}\n'.format(len(self.not_voice)))
+            [f.write(i+'\n') for i in self.not_voice]
+            f.write('\nfail : {0}\n'.format(len(self.fail)))
+            [f.write(i+'\n') for i in self.fail]
+        print('Download end '+ self.circle)
 
 
+        num=int(len(self.sample))+int(len(self.m4a))+int(len(self.not_voice))+int(len(self.fail))
+        if(self.totalworks!=num):
+            print('ERROR')
+            print('サークルの作品数とダウンロードした作品数が合いません。')
+            print('作品数{0},ダウンロード作品数{1}'.format(self.totalworks,num))
+
+    
+    #以下はインスタンスから呼び出さない
+    def get_circleName(self,soup):
+        elems=soup.find("strong",attrs={'class':'prof_maker_name'})
+        circle=elems.get_text()
+        return circle
+    def get_totalworks(self,soup):
+        elems=soup.find("div",attrs={"class":"page_total"})
+        elems=elems.find("strong")
+        totalworks=int(elems.get_text())
+        return totalworks
+    def get_list_workurls(self,soup):
+        urls=[]
+        pagenum=-(-self.totalworks//50)
+        for i in list( range(1, pagenum+1)):
+            if(i==1):
+                elems = soup.find_all('dd',attrs={'class':'work_name'})
+            else:
+                response = requests.get(self.url+'/show_type/3/page/{0}'.format(i))
+                soup = BeautifulSoup(response.text, "html.parser")
+                elems = soup.find_all('dd',attrs={'class':'work_name'})
+            
+            for elem in elems:
+                urls.append( elem.find('a')['href'] )
+        
+        #print(self.totalworks==len(urls))
+        return urls
+
+class m4a_tools:
+    def __init__(self,url):
+        self.url=url
+        self.work_id=url.split('/')[-1].split('.')[0]+'_trial'
+        self.chobit_url=self.get_chobit_url(self.url)
+        print("m4a_tools コンストラクタが呼ばれました")
+        
+    def download(self,dirpath):
+        os.makedirs(os.path.join(dirpath,self.work_id),exist_ok=True)
+        m4a_urls=self.get_m4a_urls(self.chobit_url)
+        for i in m4a_urls:
+            filename=i.split('/')[-1]
+            urllib.request.urlretrieve(i, os.path.join(dirpath,self.work_id,filename))
+            print('Download {0}'.format(filename))
+
+
+    def get_chobit_url(self,url):
+
+        #url='https://www.dlsite.com/maniax/work/=/product_id/RJ381003.html'
+
+        #立ち上げ時の制約（Option）の定義
+        ChromeOptions = webdriver.ChromeOptions()
+        #これがエラーをなくすコードです。ブラウザ制御コメントを非表示化しています
+        ChromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
+        #これがエラーをなくすコードです。WebDriverのテスト動作をTrueにしています
+        ChromeOptions.use_chromium = True
+        #ヘッドレスモード
+        ChromeOptions.add_argument('--headless')
+
+        driver = webdriver.Chrome('chromedriver_win32\chromedriver',options=ChromeOptions)
+        driver.get(url)
+        #driver.find_element_by_class_name('btn_yes').click()
+
+        html = driver.page_source.encode('utf-8')
+        soup = BeautifulSoup(html, "html.parser")
+        elems=soup.find_all('iframe',attrs={'src':re.compile(r'.*')})
+        chobit_url=''
+        for e in elems:
+            #print(e['src'])
+            if(re.search('chobit.cc',e['src'])):
+                chobit_url=e['src']
+
+        print(chobit_url)
+
+        #html書き出し
+        # with open('selenium_test.html','w',encoding='utf-8') as f:
+        #     f.write(str(html))
+        print('Complete extracting chobit url ')
+
+        return chobit_url
+
+    def get_m4a_urls(self,chobit_url):
+        urls=[]
+        response = requests.get(chobit_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        elems = soup.find_all('li')
+        for e in elems:
+            urls.append(e['data-src'])
+        #print(urls)
+        return urls
