465277fd24679d1277685356ba5a99775d44277e
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2023-04-03 04:02:15 +0900

    Amadeusのimportを変えた

--- auto_downloader.py
+++ auto_downloader.py
@@ -1,37 +1,15 @@
-from Amadeus import amadeus
+import os,sys
+
+# Amadeusのインポート
+# モジュールが含まれるディレクトリの絶対パスを取得
+module_dir = os.path.dirname(os.path.abspath("amadeus.py"))
+# 絶対パスをシステムパスに追加
+sys.path.insert(0, module_dir)
+# モジュールをインポート
+import amadeus
+
 
 urls=[
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG39707.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG35984.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG59558.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG50132.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG17947.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG69997.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG70058.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG45006.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG69796.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG70077.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG37417.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG68834.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG59215.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG42158.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG35024.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG04133.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG34479.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG47625.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG58264.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG07203.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG69893.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG56035.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG68464.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG53719.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG51111.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG63942.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG54327.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG46674.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG40158.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG45234.html',
-    'https://www.dlsite.com/maniax/circle/profile/=/maker_id/RG46130.html',
 ]
 
 for url in urls:
