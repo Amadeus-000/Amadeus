cf85c9674d8f267adaaac8ef497ad35f737b8c4d
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2023-06-10 20:44:25 +0900

    gg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Amadeus",
-    version="4.0.10",
+    version="4.0.11",
     packages=find_packages(),
     package_data={'amadeus': ['*.json'],'faure': ['*.json']},
 )
