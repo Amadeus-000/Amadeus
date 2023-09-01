280410573177b82040e4e5c697a1bd2658722a51
Amadeus-000 <ma3hi68rada@hotmail.co.jp>
2023-09-02 08:49:55 +0900

    getcircleurlのurl変更

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Amadeus",
-    version="4.3.1",
+    version="4.3.2",
     packages=find_packages(),
     package_data={'amadeus': ['*.json'],'faure': ['*.json']},
 )
